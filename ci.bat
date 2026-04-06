35090e9bfea6f636550f9c4587185fc97690b50b
maksymvatrychki2021-source <maksym.vatrych.ki.2021@lpnu.ua>
2026-04-06 09:42:20 +0300

    Update ci.bat

--- ci.bat
+++ ci.bat
@@ -2,3 +2,8 @@ git clone https://github.com/microsoft/vcpkg.git
 cd vcpkg
 bootstrap-vcpkg.bat
 vcpkg install gtest
+mkdir build
+cd build
+cmake .. -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake
+cmake --build . --config Release
+ctest --output-on-failure
